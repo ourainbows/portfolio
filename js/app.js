568458f9ced417cdef44ee19a61c0da6b8ebebc2
ourainbows <casasjuanfelipe@gmail.com>
2022-01-06 22:59:10 -0500

    Javascript to send emails

--- js/app.js
+++ js/app.js
@@ -3,3 +3,17 @@ VanillaTilt.init(document.querySelectorAll(".tech__card")), {
     speed: 400,
 
 }
+
+
+/* Formulario */
+let sendEmail = document.querySelector("#sendEmail")
+sendEmail.addEventListener("click", function (e) {
+    /* Don't reload page when button is press */
+    e.preventDefault()
+    /* Obtain input values */
+    let name = document.getElementById("nameForm").value
+    let email = document.getElementById("emailForm").value
+    let message = document.getElementById("textForm").value
+
+    window.location.href = `mailto:casasjuanfelipe@gmail.com?subject=${name}, interesado en contactarte&body=Email: ${email}%0A${message}`
+})
